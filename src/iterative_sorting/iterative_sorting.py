608d05c0cff1000889866fe252644faab868790c
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-09-30 22:23:53 -0700

    mvp completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,17 +7,31 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range (i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+
+        if i != smallest_index:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        for i in range(0, len(arr) - 1):
+
+            if arr[i + 1] < arr[i]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -8,7 +10,21 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        middle = (low + high) // 2
+
+        if arr[middle] == target:
+            return middle
+
+        if arr[middle] < target:
+            low = middle + 1
+            
+
+        if arr[middle] > target:
+            high = middle - 1
+            
 
     return -1  # not found
